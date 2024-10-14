e42bc8282f9edf677c2f053fe58b12244e1bc18b
coverfrog <justfroglab@gmail.com>
2024-10-15 01:02:07 +0900

    패키지 업데이트

--- Assets/__Core/1_Scripts/Input/Runtime/InputCtrl.cs
+++ Assets/__Core/1_Scripts/Input/Runtime/InputCtrl.cs
@@ -4,18 +4,8 @@ using UnityEngine;
 
 namespace Cf.Input
 {
-    public class InputCtrl : MonoBehaviour
+    public abstract class InputCtrl : MonoBehaviour
     {
-        // Start is called before the first frame update
-        void Start()
-        {
         
-        }
-
-        // Update is called once per frame
-        void Update()
-        {
-        
-        }
     }
 }
